1700fa88d168d35bed5f56d18e967175a57bae9d
DavideFolk <107867374+DavideFolk@users.noreply.github.com>
2022-10-13 08:37:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function pokemonRender(pokemon) {
+	axios
+		.get('https://pokeapi.co/api/v2/pokemon/' + pokemon)
+		.then(function (response) {
+			let nome = response.data.forms[0].name;
+			let nomeCorretto = nome.at(0).toUpperCase() + nome.slice(1);
+			let numero = response.data.id;
+			let tipo = response.data.types[0].type.name;
+			let altezza = response.data.height * 10;
+			let linkImmagine =
+				response.data.sprites.other['official-artwork'].front_default;
+			let peso = response.data.weight / 10;
+			$('#nome').text(nomeCorretto);
+			$('#numero').text(`n. ${numero}`);
+			$('#tipo').text(`Tipo: ${tipo}`);
+			$('#altezza').text(`Altezza: ${altezza} cm`);
+			$('#immagine').prop('src', linkImmagine);
+			$('#peso').text(`Peso: ${peso} kg`);
+			$('#card').removeClass('hidden');
+		})
+		.catch(function (e) {
+			console.log('errore', e);
+		});
+
+	axios
+		.get('https://pokeapi.co/api/v2/pokemon-species/' + pokemon)
+		.then(function (response) {
+			let testo = response.data.flavor_text_entries;
+			//$('#testo').text(testo);
+			for (const k in testo) {
+				if (testo[k].language.name == 'it') {
+					$('#testo').text(testo[k].flavor_text);
+					break;
+				}
+			}
+		})
+		.catch(function (e) {
+			console.log('errore', e);
+		});
+}
+
+$('#button-addon2').click('submit', function (e) {
+	e.preventDefault();
+	let pokemon = $('#pkmSearch').val();
+	pokemonRender(pokemon);
+});
+
+$('#button-random').click((e) => {
+	e.preventDefault();
+	let nPokemon = Math.floor(Math.random() * 905 + 1);
+	pokemonRender(nPokemon);
+});
