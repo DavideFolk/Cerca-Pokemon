fbfa5b70b03bebfdeadb522d997d7a339b005710
DavideFolk <107867374+DavideFolk@users.noreply.github.com>
2022-10-13 17:32:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function pokemonRender(pokemon) {
 	axios
 		.get('https://pokeapi.co/api/v2/pokemon/' + pokemon)
 		.then(function (response) {
+			$('#card').addClass('hidden');
+			$('#cardFail').addClass('hidden');
 			let nome = response.data.forms[0].name;
 			let nomeCorretto = nome.at(0).toUpperCase() + nome.slice(1);
 			let numero = response.data.id;
@@ -10,16 +12,26 @@ function pokemonRender(pokemon) {
 			let linkImmagine =
 				response.data.sprites.other['official-artwork'].front_default;
 			let peso = response.data.weight / 10;
+			$('#loader').removeClass('hidden');
+			setTimeout(() => {
+				$('#loader').addClass('hidden');
+				$('#card').removeClass('hidden');
+			}, 600);
 			$('#nome').text(nomeCorretto);
 			$('#numero').text(`n. ${numero}`);
 			$('#tipo').text(`Tipo: ${tipo}`);
 			$('#altezza').text(`Altezza: ${altezza} cm`);
 			$('#immagine').prop('src', linkImmagine);
 			$('#peso').text(`Peso: ${peso} kg`);
-			$('#card').removeClass('hidden');
 		})
 		.catch(function (e) {
-			console.log('errore', e);
+			$('#card').addClass('hidden');
+			$('#cardFail').addClass('hidden');
+			$('#loader').removeClass('hidden');
+			setTimeout(() => {
+				$('#loader').addClass('hidden');
+				$('#cardFail').removeClass('hidden');
+			}, 600);
 		});
 
 	axios
